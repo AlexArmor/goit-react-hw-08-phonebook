77c488244b3ced0d6c2fdf94af2e3ce0cdc99d9a
Oleksandr Bron <lexlutermotion@gmail.com>
2023-01-21 22:26:41 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,26 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactReducer } from './contactSlice';
 import { filterReducer } from './filterSlice';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
 
 export const store = configureStore({
   reducer: {
