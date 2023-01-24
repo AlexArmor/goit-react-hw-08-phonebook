a897153f9bdf0711af9f4e5c5d5a4f5cdc508255
AlexArmor <113610104+AlexArmor@users.noreply.github.com>
2023-01-24 02:11:36 +0000

    Deploying to gh-pages from @ AlexArmor/goit-react-hw-08-phonebook@c41db6a785a6a24e026a9331c3dfe3d2875e1691 🚀

---
+++ static/js/415.60df343a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n),o.d(n,{default:function(){return h}});var t=o(1614),c=o(890),r=o(184);function h(){return(0,r.jsx)(t.Z,{children:(0,r.jsxs)(c.Z,{variant:"h3",component:"h1",sx:{textAlign:"center",paddingTop:"60px"},children:["Phonebook welcome page"," "]})})}}}]);
+//# sourceMappingURL=415.60df343a.chunk.js.map
